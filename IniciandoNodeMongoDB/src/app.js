4f0c83f1193780a1c1a2f5faa5ac7cd555dfb3d4
guDutra <gudutra05@outlook.com>
2023-05-09 02:34:22 -0300

    Subindo projeto pronto

--- IniciandoNodeMongoDB/src/app.js
+++ IniciandoNodeMongoDB/src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import db from './config/dbConnect.js'
+import livros from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, 'Erro de conexãoc com o banco'));
 db.once("open", () => {
@@ -9,39 +11,6 @@ const app = express();
 
 app.use(express.json());
 
-const livros = [
-    { id: 1, "titulo": "Senhor dos Aneis" },
-    { id: 2, "titulo": "O Hobbit" }
-]
+routes(app);
 
-app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node');
-})
-
-app.get('/livros', (req, res) => {
-    res.status(200).json(livros)
-})
-
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Livro foi cadastrado com sucesso')
-})
-
-app.get('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).send(livros[index]);
-})
-
-app.delete('/livros/:id', (req, res) => {
-    let { id } = req.params;
-    let index = buscaLivro(id);
-    livros.splice(index, 1);
-    res.send(`Livro ${id} removido com sucesso`);
-})
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => livro.id == id)
-}
 export default app
